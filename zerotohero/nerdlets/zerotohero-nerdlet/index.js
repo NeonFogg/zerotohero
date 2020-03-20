2b6845c82277fc0ba66ed56e9bccf0749d1dfa4d
silverfogdescent <0123silas@gmail.com>
2020-03-20 15:46:11 +0000

    NerdletProject

--- zerotohero/nerdlets/zerotohero-nerdlet/index.js
+++ zerotohero/nerdlets/zerotohero-nerdlet/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 // https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
 
-export default class ZerotoheroNerdletNerdlet extends React.Component {
+export default class Nerdlet extends React.Component {
     render() {
-        return <h1>Hello, zerotohero-nerdlet Nerdlet!</h1>;
+        return <h1>Hello, This is the Zero to Hero nerdlet!</h1>;
     }
 }
